cc4617340be18b10b1a24b736b6f463c2ecb3758
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-18 16:51:54 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,6 +1,12 @@
 // your ImageSlider code here!
 
 export default class ImageSlider extends Component {
-  
+  constructor(){
+    super(); 
+    
+    this.state = {
+      currentSlideIndex = 0; 
+    }
+  }
   
 }
